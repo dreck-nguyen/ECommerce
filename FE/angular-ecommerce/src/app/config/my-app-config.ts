5e28484c17b0b0a9e73db49fd709633b7bcf5281
DUCHNH <hduy01012000@gmail.com>
2023-03-10 17:27:10 +0700

    Install Okta Dependencies

---
+++ FE/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa8nrmldfDaJLari5d7',
+    issuer: 'https://dev-84011879.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
